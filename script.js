4a7fd7df6016a516d35d6fb98250521a5093dcb6
Rifat-webdesigner <86959249+Rifat-webdesigner@users.noreply.github.com>
2021-08-09 14:57:48 +0600

    update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ window.addEventListener("scroll", reveal);
 function reveal(){
   var reveals = document.querySelectorAll(".reveal");
 
-  for(var i = 0; i < reveals.length; i++){
+  for(var i = 1; i < reveals.length; i++){
     var windowHeight = window.innerHeight;
     var revealTop = reveals[i].getBoundingClientRect().top;
     var revealPoint = 50;
